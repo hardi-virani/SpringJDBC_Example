6ff5d58c7b37b146774b39200de5965bb72d9715
Hardi <hardivirani2003@gmail.com>
2026-06-05 12:21:26 -0400

    Created the JDBC template

--- src/main/java/com/star/SpringJDBCEx/repo/StudentRepo.java
+++ src/main/java/com/star/SpringJDBCEx/repo/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.star.SpringJDBCEx.repo;
 
 import com.star.SpringJDBCEx.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,10 +11,25 @@ import java.util.List;
 @Repository
 public class StudentRepo {
 
+    //Reference for JDBC template
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
 
     public void save(Student s) {
 
-        System.out.println("Added..");
+//        System.out.println("Added..");
+
+        String sql = "insert into student(rollno, name, marks) values(?,?,?)";
+        int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getMarks());
+        System.out.println(rows + "effected");
     }
 
     public List<Student> findAll() {
