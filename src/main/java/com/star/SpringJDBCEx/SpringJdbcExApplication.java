fd80b119f482ed6d31f26a413916157c767015bc
Hardi <hardivirani2003@gmail.com>
2026-06-05 08:57:48 -0400

    Student Managment Project.

--- src/main/java/com/star/SpringJDBCEx/SpringJdbcExApplication.java
+++ src/main/java/com/star/SpringJDBCEx/SpringJdbcExApplication.java
@@ -1,13 +1,23 @@
 package com.star.SpringJDBCEx;
 
+import com.star.SpringJDBCEx.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcExApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringJdbcExApplication.class, args);
+
+		Student s = context.getBean(Student.class);
+		s.setRollNo(101);
+		s.getName("Navin");
+		s.getMarks(78);
+
+		addStudent(s);
+
 	}
 
 }
